1e73257f49a9e01a7cbfa47190e962fe0b9ac9f9
vibracode <Aliza@DESKTOP-D9RC250>
2021-09-08 11:48:41 +0430

    first comit to start project

--- fundamentals exersises.cpp
+++ fundamentals exersises.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main()
 {
 
-	//write code here
+	cout << "starting project with first comit";
+	cout << "hello programmers";
 
 	getchar(); //to prevent console from exiting if needed
 	return 0;
