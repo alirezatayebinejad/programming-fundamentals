7895057a03e2d289e4224deacac03d8a35f1dfde
vibracode <Aliza@DESKTOP-D9RC250>
2021-09-08 17:46:18 +0430

    example 5 coded

--- fundamentals exersises.cpp
+++ fundamentals exersises.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+/*
 // example 1
 int main()
 {
@@ -20,3 +21,92 @@ int main()
 	cin.get();
 	return 0;
 }
+*/
+
+/*
+// example 2
+int main()
+{
+	int a,mohit,masahat;
+	cin >> a;
+	mohit = a * 4;
+	masahat = a * a;
+	cout << "masahat: " << masahat << endl;
+	cout << "mohit: " << mohit << endl;
+	
+	cin.get(); //to prevent from closing the console if needed
+	cin.get();
+	return 0;
+}
+*/
+
+/*
+//example 3
+int main()
+{
+	int arz,tool, mohit, masahat;
+	cout << "arz ra vared kon:\n";
+	cin >> arz;
+	cout << "tool ra vared kon:\n";
+	cin >> tool;
+	mohit = (arz + tool) * 2;
+	masahat = arz * tool;
+	cout << "masahat: " << masahat << endl;
+	cout << "mohit: " << mohit << endl;
+
+
+	cin.get(); //to prevent from closing the console if needed
+	cin.get();
+	return 0;
+}
+*/
+/*
+//example 4
+int main()
+{
+	float shoa, mohit, masahat;
+
+	cout << "shoa dayere ra vared kon:\n";
+	cin >> shoa;
+
+	mohit = 2*3.14*shoa; // 3.14 adad pi hast
+	masahat = 3.14*shoa*shoa;
+	cout << "masahat: " << masahat << endl;
+	cout << "mohit: " << mohit << endl;
+
+
+	cin.get(); //to prevent from closing the console if needed
+	cin.get();
+	return 0;
+}
+*/
+/*
+//example 5
+int main()
+{
+	float zfizik, nfizik, zriazi, nriazi, zamar, namar;
+	cout << "nomre va zarib riazi fizik va amar ra vared konid: \n";
+
+	cout << "fizik\n" << "zarib:";
+	cin >> zfizik;
+	cout << "nomre:";
+	cin >> nfizik;
+
+	cout << "riazi\n" << "zarib:";
+	cin >> zriazi;
+	cout << "nomre:";
+	cin >> nriazi;
+
+	cout << "amar\n" << "zarib:";
+	cin >> zamar;
+	cout << "nomre:";
+	cin >> namar;
+
+	float moadel = ((zfizik * nfizik) + (zriazi * nriazi) + (zamar * namar)) / (zfizik + zriazi + zamar);
+
+	cout << "moadel shoma: "<< moadel;
+	cin.get(); //to prevent from closing the console if needed
+	cin.get();
+	return 0;
+}
+*/
